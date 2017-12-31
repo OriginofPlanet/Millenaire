7be9a4286f01303c9781a312d49a054bfbc6aacb
Benjamin Sims <asschaps@outlook.com>
2017-12-31 11:41:40 -0600

    Misc changes, some texture fixes

--- src/main/java/org/millenaire/blocks/BlockDecorativeStone.java
+++ src/main/java/org/millenaire/blocks/BlockDecorativeStone.java
@@ -39,11 +39,8 @@ public class BlockDecorativeStone extends Block
         if (Block.getBlockFromItem(itemIn) == this)
         {
             BlockDecorativeStone.EnumType[] aenumtype = BlockDecorativeStone.EnumType.values();
-            int i = aenumtype.length;
 
-            for (int j = 0; j < i; ++j)
-            {
-            	BlockDecorativeStone.EnumType enumtype = aenumtype[j];
+            for (EnumType enumtype : aenumtype) {
                 list.add(new ItemStack(itemIn, 1, enumtype.getMetadata()));
             }
         }
@@ -67,7 +64,7 @@ public class BlockDecorativeStone extends Block
     }
 
     @Override
-    protected BlockState createBlockState() { return new BlockState(this, new IProperty[] {VARIANT}); }
+    protected BlockState createBlockState() { return new BlockState(this, VARIANT); }
 
     //////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
@@ -108,11 +105,8 @@ public class BlockDecorativeStone extends Block
         static
         {
         	BlockDecorativeStone.EnumType[] var0 = values();
-            int var1 = var0.length;
 
-            for (int var2 = 0; var2 < var1; ++var2)
-            {
-            	BlockDecorativeStone.EnumType var3 = var0[var2];
+            for (EnumType var3 : var0) {
                 META_LOOKUP[var3.getMetadata()] = var3;
             }
         }
